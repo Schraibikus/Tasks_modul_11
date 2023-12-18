2407cbbe593d8173e23009d7b5a0eeea069cac5b
Schraibikus <kurt-77@mail.ru>
2023-12-19 00:02:19 +0500

    add task_2

--- task_1.js
+++ task_1.js
@@ -7,41 +7,44 @@
 
 console.log('Задача 1');
 const array1 = [1, 2, -10001, 4, 6, 7, null, 0, 0, 0, '10', -6, true];
-console.log(array1);
+const array2 = [-52, -20, 0, null, '12', false, 99];
 
-function getEvenOddElementsFromArray() {
+function getEvenOddElementsFromArray(array) {
 	let noNumber = 0
 	let nullNumber = 0;
 	let evenNumber = 0;
 	let oddNumber = 0;
 
-	for (let i = 0; i < array1.length; i++) {
-		if (typeof array1[i] !== 'number') {
+	for (let i = 0; i < array.length; i++) {
+		if (typeof array[i] !== 'number') {
 			noNumber++;
-			console.log(`элемент ${array1[i]}, под индексом ${i} не является числом`);
-		} else if (array1[i] === 0) {
+			// console.log(`элемент ${array[i]}, под индексом ${i} не является числом`);
+		} else if (array[i] === 0) {
 			nullNumber++;
-			console.log(`элемент ${array1[i]}, под индексом ${i} является нулём`);
-		} else if (array1[i] % 2 === 0) {
+			// console.log(`элемент ${array[i]}, под индексом ${i} является нулём`);
+		} else if (array[i] % 2 === 0) {
 			evenNumber++;
-			console.log(`элемент ${array1[i]}, под индексом ${i} чётное число`);
-		} else if (array1[i] % 2 !== 0) {
+			// console.log(`элемент ${array[i]}, под индексом ${i} чётное число`);
+		} else if (array[i] % 2 !== 0) {
 			oddNumber++;
-			console.log(`элемент ${array1[i]}, под индексом ${i} нечётное число`);
+			// console.log(`элемент ${array[i]}, под индексом ${i} нечётное число`);
 		} else {
 			console.log('Ну тогда я не знаю как решать, опыта маловато');
 		}
 	}
 	console.log('\n');
-	console.log(`Всего элементов в массиве: ${array1.length}`);
+	console.log(`Всего элементов в массиве: ${array.length}`);
 	console.log(`Всего элементов не являющихся числами: ` + noNumber);
 	console.log(`Всего элементов являющихся нулём: ` + nullNumber);
 	console.log(`Всего чётных элементов: ` + evenNumber);
 	console.log(`Всего нечётных элементов: ` + oddNumber);
 }
 
-getEvenOddElementsFromArray();
+getEvenOddElementsFromArray(array1);
 console.log('\n');
+getEvenOddElementsFromArray(array2);
+console.log('\n');
+
 
 
 
